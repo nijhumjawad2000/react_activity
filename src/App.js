1849104946158d0557f9575025aadb1ead4d27ef
nijhum_jawad2000 <njawad@techlaunch.io>
2019-03-04 21:04:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Pink Rose</h1>
+        <p>A beautiful flower</p>
+        <img src="https://cdn.pixabay.com/photo/2018/02/09/21/46/rose-3142529__340.jpg" />
+      </div>
+    );
+  }
+}
+
+export default App;
